100b79fd37009eab076b57a17337f12beda585b5
Siva311096 <mailnow4siva3110@gmail.com>
2022-02-15 20:09:20 +0530

    a

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -4,5 +4,7 @@ public class AxisBank {
 	public void deposit() {
 		System.out.println("total deposit amount in bank is 100 lakh");
 	}
-
+public void aPerson() {
+System.out.println("first merge");
+}
 }
